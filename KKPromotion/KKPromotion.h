f73c6d94d3ff6d9034dd591bae8ed1ce790be26e
Bear.wang <wyx96553@163.com>
2020-02-04 15:39:09 +0800

    调整工程结构

---
+++ KKPromotion/KKPromotion.h
@@ -0,0 +1,18 @@
+//
+//  KKPromotion.h
+//  KKPromotion
+//
+//  Created by aby on 2020/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KKPromotion.
+FOUNDATION_EXPORT double KKPromotionVersionNumber;
+
+//! Project version string for KKPromotion.
+FOUNDATION_EXPORT const unsigned char KKPromotionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKPromotion/PublicHeader.h>
+
+
---
+++ examples/KKPromotion-Objective-C/KKPromotion-Objective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKPromotion-Objective-C
+//
+//  Created by aby on 2020/2/4.
+//  Copyright © 2020 aby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
